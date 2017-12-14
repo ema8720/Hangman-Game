fb28efe154a87840e86af81b5f839d9a2267fa49
Juan Correa <ema8720@gmail.com>
2017-12-13 20:39:11 -0800

    Hangman with images added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,7 +49,7 @@ function startGame() {
 
 function checkLetters(letter) {
     // Check if letter exist i code at all
-    alert(letter);
+    // alert(letter);
     var isLetterInWord = false;
 
     for (var i = 0; i < numBlanks; i++) {
@@ -86,10 +86,31 @@ function roundComplete() {
     //Check if user won
     if (lettersInWord.toString() == blanksAndSuccesses.toString()) {
         winCount++;
-        alert("You won!")
-            // Update the win counter in the html
+        // document.getElementById("pic").setPointerCapture = "assets/images/madonna.png";
+        // alert("You won!")
+        // Update the win counter in the html
         document.getElementById("winCounter").innerHTML = winCount;
+        if (selectedWord === wordOptions[7]) {
+            document.getElementById("pic").src = 'assets/images/madonna.png';
+        } else if (selectedWord === wordOptions[6]) {
+            document.getElementById("pic").src = 'assets/images/ironmaiden.png';
+        } else if (selectedWord === wordOptions[5]) {
+            document.getElementById("pic").src = 'assets/images/johnlennon.png';
+        } else if (selectedWord === wordOptions[4]) {
+            document.getElementById("pic").src = 'assets/images/eltonjohn.png';
+        } else if (selectedWord === wordOptions[3]) {
+            document.getElementById("pic").src = 'assets/images/bonjovi.png';
+        } else if (selectedWord === wordOptions[2]) {
+            document.getElementById("pic").src = 'assets/images/queen.png';
+        } else if (selectedWord === wordOptions[1]) {
+            document.getElementById("pic").src = 'assets/images/davidbowie.png';
+        } else {
+            document.getElementById("pic").src = 'assets/images/michaeljackson.png';
+        }
         startGame();
+        // Else if statements
+
+
     }
     // Check if user lost
     else if (guessesLeft == 0) {
@@ -100,6 +121,9 @@ function roundComplete() {
         startGame();
     }
 }
+
+
+
 
 // Main Process
 //==============================================
